0b0753551cc07430b7bb4f12259da9d3d05831f9
john-chang94 <j.chang94@yahoo.com>
2020-08-07 22:27:50 -0700

    set up sendgrid

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User'); // Placed before passport because passport function looks for a mongoose model inside it
+require('./models/Survey');
 require('./services/passport'); // Shortened because there is no var assigned in this file
 
 mongoose.connect(keys.MONGO_URI);
@@ -22,6 +23,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
---
+++ services/Mailer.js
@@ -0,0 +1,61 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+const { response } = require('express');
+
+class Mailer extends helper.Mail {
+    // Constructor allows us to call 'new Mailer()' in surveyRoutes | FUNDAMENTALS!
+    constructor({ subject, recipients }, content) {
+        super();
+
+        this.sgApi = sendgrid(keys.SENDGRID_KEY);
+        this.from_email = new helper.Email('changjohn94@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body); // addContent comes from helper
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses = recipients => {
+        return recipients.map(({ email }) => {
+            // Return an array with individual emails
+            return new helper.Email(email);
+        })
+    }
+
+    // This is required for sendgrid tracking
+    addClickTracking = () => {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients = () => {
+        const personalize = new helper.Personalization();
+
+        this.recipients.forEach(recipient => {
+            // For each recipient, add to personalize object
+            personalize.addTo(recipient);
+        })
+        // Add the personalized object to addPersonalization method from helper
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        })
+
+        this.sgApi.API(request); // API from sendgrid
+        return response;
+    }
+}
+
+module.exports = Mailer;
