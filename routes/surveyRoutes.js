d746162536dd1440b8dad7b83374358afbf5070a
john-chang94 <j.chang94@yahoo.com>
2020-08-17 23:49:16 -0700

    updated survey route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url'); // No npm necessary
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,8 +10,51 @@ const surveyTemplate = require('../services/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for your feedback!');
+    })
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        _.chain(req.body)
+        // url and email are properties from sendgrid response
+            .map(({ email, url }) => {
+                // Cannot destructure match if objected returned is null
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice }
+                }
+            })
+            .compact() // Remove undefined responses
+            .uniqBy('email', 'surveyId') // Remove responses with duplicate values for BOTH email and survey
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false }
+                    }
+                }, {
+                    // Increment the 'yes' or 'no' property by 1
+                    $inc: { [choice]: 1 },
+                    // $ is like the index of the record that was just found above
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value();
+
+        // const events = _.map(req.body, (event) => {
+        //     // .url is a property from sendgrid response
+        //     // .pathname extracts path after domain
+        //     const pathname = new URL(event.url).pathname;
+        //     const match = p.test(pathname);
+        //     if (match) {
+        //         return { email: event.email, surveyId: match.surveyId, choice: match.choice };
+        //     }
+        // })
+
+        // const compactEvents = _.compact(events);
+        // const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
     })
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
